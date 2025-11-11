abcf8e43b64f54df0410254674d9f8a360fafce7
Loic Gouarin <loic.gouarin@gmail.com>
2025-11-11 08:33:06 +0100

    add Euler skeleton

---
+++ practical_session/material/04-euler/euler/initial_condition.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <numbers>
+
+#include "variables.hpp"
+
+auto riemann_config_3(auto& u)
+{
+    double x0 = 0.5;
+    double y0 = 0.5;
+
+    PrimState<2> quad1_state{
+        1.5,
+        1.5,
+        xt::xtensor_fixed<double, xt::xshape<2>>{0., 0.}
+    };
+
+    PrimState<2> quad2_state{
+        0.5323,
+        0.3,
+        xt::xtensor_fixed<double, xt::xshape<2>>{1.206, 0.}
+    };
+
+    PrimState<2> quad3_state{
+        0.138,
+        0.29,
+        xt::xtensor_fixed<double, xt::xshape<2>>{1.206, 1.206}
+    };
+
+    PrimState<2> quad4_state{
+        0.5323,
+        0.3,
+        xt::xtensor_fixed<double, xt::xshape<2>>{0, 1.206}
+    };
+
+    samurai::for_each_cell(u.mesh(),
+                           [&](auto& cell)
+                           {
+                               auto x = cell.center();
+
+                               if (x[0] >= x0 && x[1] >= y0)
+                               {
+                                   u[cell] = prim2cons<2>(quad1_state);
+                               }
+                               else if (x[0] < x0 && x[1] >= y0)
+                               {
+                                   u[cell] = prim2cons<2>(quad2_state);
+                               }
+                               else if (x[0] < x0 && x[1] < y0)
+                               {
+                                   u[cell] = prim2cons<2>(quad3_state);
+                               }
+                               else // (x[0] >= x0 && x[1] < y0)
+                               {
+                                   u[cell] = prim2cons<2>(quad4_state);
+                               }
+                           });
+}
---
+++ practical_session/material/04-euler/main.cpp
@@ -0,0 +1,55 @@
+#include <numbers>
+
+#include <samurai/field.hpp>
+#include <samurai/io/hdf5.hpp>
+#include <samurai/mr/mesh.hpp>
+#include <samurai/samurai.hpp>
+
+#include "euler/config.hpp"
+#include "euler/initial_condition.hpp"
+#include "euler/utils.hpp"
+#include "euler/variables.hpp"
+
+int main(int argc, char* argv[])
+{
+    constexpr std::size_t dim = 2;
+    using mesh_t              = config<dim>::mesh_t;
+
+    samurai::initialize("Euler example", argc, argv);
+    SAMURAI_PARSE(argc, argv);
+
+    // Multiresolution parameters
+    std::size_t min_level = 8;
+    std::size_t max_level = 8;
+
+    // Initialize the mesh
+    auto box = ; // TO IMPLEMENT
+
+    mesh_t mesh; // TO IMPLEMENT
+    auto u = ;   // TO IMPLEMENT
+    riemann_config_3(u);
+
+    double Tf      = .25;
+    double cfl     = 0.4;
+    double t       = 0.;
+    std::size_t nt = 0;
+    double dx      = mesh.cell_length(max_level);
+
+    samurai::save("euler_init", mesh, u);
+
+    while (t != Tf)
+    {
+        double dt = cfl * dx / get_max_lambda(u);
+        t += dt;
+
+        if (t > Tf)
+        {
+            dt += Tf - t;
+            t = Tf;
+        }
+        std::cout << fmt::format("iteration {}: t = {}, dt = {}", nt++, t, dt) << std::endl;
+    }
+
+    samurai::finalize();
+    return 0;
+}
